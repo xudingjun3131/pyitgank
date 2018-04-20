1de6898a1d72388600fc3a59619da9bf0413f9cb
xudingjun <xudingjun3131@163.com>
2018-04-20 09:48:09 +0800

    init

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     #url(r'^login/$', views.user_login, name='user_login'),
     url(r'^login/$', auth_views.login, name='user_login'),
     url(r'^new-login/$', auth_views.login, {"template_name": "account/login.html"}),
+    #url(r'^logout/$', auth_views.logout, name='user_logout'),
+    url(r'^logout/$', auth_views.logout,{"template_name": "account/logout.html"}, name='user_logout'),
 ]
