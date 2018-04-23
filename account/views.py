65e3aa097648dd19e5ffdd2639c2cee07f569504
xudingjun <xudingjun3131@163.com>
2018-04-23 14:49:35 +0800

    init

--- account/views.py
+++ account/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 # Create your views here.
 
+#定义用户登陆
 def user_login(request):
     if request.method == "POST":
         login_form = LoginForm(request.POST)
@@ -22,3 +23,18 @@ def user_login(request):
     if request.method == "GET":
         login_form = LoginForm()
         return render(request, "account/login.html", {"form": login_form})
+
+#定义用户注册
+def register(request):
+    if request.method == "POST":
+        user_form = RegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return HttpResponse("successfully!")
+        else:
+            return HttpResponse("sorry, you can not register.")
+    else:
+        user_form = RegistrationForm()
+        return render(request, "account/register.html", {"form": user_form})
