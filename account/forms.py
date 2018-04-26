bcc2c379565d0337cb59ec6ef4bd3749baa8fa47
xudingjun <xudingjun3131@163.com>
2018-04-26 17:21:33 +0800

    user login

--- account/forms.py
+++ account/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import UserProfile
 
 #定义登陆表单
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '用户名', }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '密码', }))
 
 #定义注册表单
 class RegistrationForm(forms.ModelForm):
@@ -17,5 +18,11 @@ class RegistrationForm(forms.ModelForm):
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
-            raise forms.ValidationError("passwrods does not match.")
+            raise forms.ValidationError("passwords does not match.")
         return cd['password2']
+
+#定义注册额外信息
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ("phone", "birth")
--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, RegistrationForm
+from .forms import LoginForm, RegistrationForm, UserProfileForm
 # Create your views here.
 
 #定义用户登陆
@@ -14,7 +14,7 @@ def user_login(request):
 
             if user:
                 login(request, user)
-                return HttpResponse("Wellcome You. You have been authenticated successfully")
+                return render(request, "blog/content.html")
             else:
                 return HttpResponse("Sorry, Your username or password is not right.")
         else:
@@ -22,19 +22,25 @@ def user_login(request):
 
     if request.method == "GET":
         login_form = LoginForm()
-        return render(request, "account/login.html", {"form": login_form})
+        return render(request, "registration/login.html", {"form": login_form})
 
 #定义用户注册
 def register(request):
     if request.method == "POST":
         user_form = RegistrationForm(request.POST)
+        userprofile_form = UserProfileForm(request.POST)
         if user_form.is_valid():
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
+            new_profile = userprofile_form.save(commit=False)
+            new_profile.user = new_user
+            new_profile.save()
             return HttpResponse("successfully!")
         else:
             return HttpResponse("sorry, you can not register.")
     else:
         user_form = RegistrationForm()
-        return render(request, "account/register.html", {"form": user_form})
+        userprofile_form = UserProfileForm()
+        return render(request, "account/register.html", {"form": user_form, "profile": userprofile_form})
+
